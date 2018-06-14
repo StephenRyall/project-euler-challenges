567815b67134db1c0e57a70a28b5777a69be8052
Steven <stevenryall812@gmail.com>
2018-06-14 10:35:48 +0100

    Sub-string-div update

--- sub-string-div.js
+++ sub-string-div.js
@@ -22,4 +22,37 @@ function permut(string) {
 
 function searchPerms() {
   let permArray = permut("1234567890");
+  let newPerms = permArray.filter(function(x) {
+    let d1 = parseInt(x.slice(1, 4));
+    let d2 = parseInt(x.slice(2, 5));
+    let d3 = parseInt(x.slice(3, 6));
+    let d4 = parseInt(x.slice(4, 7));
+    let d5 = parseInt(x.slice(5, 8));
+    let d6 = parseInt(x.slice(6, 9));
+    let d7 = parseInt(x.slice(7, 10));
+    // console.log(x);
+    if (
+      d1 % 2 == 0 &&
+      d2 % 3 == 0 &&
+      d3 % 5 == 0 &&
+      d4 % 7 == 0 &&
+      d5 % 11 == 0 &&
+      d6 % 13 == 0 &&
+      d7 % 17 == 0
+    ) {
+      return true;
+    }
+  });
+
+  return newPerms;
 }
+
+function addUp() {
+  let filterArray = searchPerms();
+  let middleArray = filterArray.map(x => parseInt(x));
+  let finalArray = middleArray.reduce((a, b) => a + b);
+
+  return finalArray;
+}
+
+console.log(addUp());
